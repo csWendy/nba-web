c96c2c5b93bf2551be92728cd0c2e9efb213fee9
Wendy <wendy.wang55@gmail.com>
2019-10-19 22:10:42 -0400

    add Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { TopNavBar } from './TopNavBar';
-import { Main } from './Main';
+import { ShotChart } from './ShotChart';
+import { Profile } from './Profile';
+import nba from 'nba';
 
-function App() {
-    return (
-        <div className="App">
-            <TopNavBar />
-            <Main />
-        </div>
-    );
+export class Main extends React.Component {
+    state = {
+        playerInfo: {},
+    }
+    componentDidMount() {
+        window.nba = nba;
+        nba.stats.playerInfo({ PlayerID: 201939})
+            .then((response) => {
+                console.log(response);
+                const { commonPlayerInfo, playerHeadlineStats } = response;
+                const playerInfo = { ...commonPlayerInfo[0], ...playerHeadlineStats[0] };
+                console.log(playerInfo);
+                this.setState({ playerInfo });
+            });
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <Profile playerInfo={this.state.playerInfo} />
+                <ShotChart playerId={201939} />
+            </div>
+        );
+    }
 }
-
-export default App;
